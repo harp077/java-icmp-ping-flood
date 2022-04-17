510cb32fe3294e4f3aa5c114b1229c0c8227c052
root <root@localhost.localdomain>
2022-04-17 22:03:09 +0400

    CREATE

---
+++ src/main/java/my/harp07/ping/flood/NewMain.java
@@ -0,0 +1,85 @@
+package my.harp07.ping.flood;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
+import javax.swing.JOptionPane;
+import org.apache.commons.validator.routines.InetAddressValidator;
+
+public class NewMain {
+
+    private static boolean pingEnabled = false;
+    private static String ip;
+    private static long N = 0;
+    private static long M = 0;
+    private static int time = 100;
+    private static ConcurrentHashMap<Integer, String> chm = new ConcurrentHashMap<>();
+    private static long run;
+    private static long end;
+    //private static long k = 0;
+    public static InetAddressValidator validIP = InetAddressValidator.getInstance();
+    private static int smp;
+
+    // ВИСНЕТ КОГДА НЕ РАБОТАЕТ DNS И БЛОКИ CATCH ПУСТЫЕ !!!
+    public static Boolean pingIp(String ipad, int timeout) {
+        try {
+            return InetAddress.getByName(ipad).isReachable(timeout);
+        } catch (UnknownHostException ex) {
+            System.out.println("//_ping_DNS-error_UnknownHostException=" + ex.getMessage());
+            //pingEnabled = false;
+        } catch (IOException | NullPointerException ex) {
+            System.out.println("//_ping_IOException/NullPointerException=" + ex.getMessage());
+            //pingEnabled = false;
+        }
+        return false;
+    }
+
+    public synchronized static void main(String[] args) {
+        ip = JOptionPane.showInputDialog("Input target IP:");
+        if (!validIP.isValid(ip)) {
+            JOptionPane.showMessageDialog(null, "Wrong input IP !", "Input Error !", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        System.out.println("ONLY FOR CHECK NETWORK NODES DEFENCE ! \nNOT FOR ATTACK !");
+        smp=Runtime.getRuntime().availableProcessors();
+        if (smp > 1) {
+            System.out.println("Use parallel calculation: Detected CPU's = "+smp);
+        }
+        System.out.println("Use small packets - for example: linux=32 byte, win=64 byte, \nFlood-ping running.  Enter 'stop' to stop:");
+        for (int j = 1; j < 999; j++) {
+            chm.put(j, ip);
+        }
+        pingEnabled = true;
+        Scanner sc = new Scanner(System.in);
+        run = System.currentTimeMillis();
+        new Thread(() -> {
+            while (pingEnabled) {
+                chm.values().parallelStream()
+                        .forEach(x -> {
+                            if (pingIp(x, time)) {
+                                N++;
+                            } else {
+                                M++;
+                            }
+                        });
+                //end = System.currentTimeMillis();
+                //System.out.println("> "+1000 * (M + N) / (end - run) + " pps");
+            }
+        }).start();
+        //System.out.println("\n");
+        while (pingEnabled) {
+            if (sc.next().toLowerCase().trim().equals("stop")) {
+                pingEnabled = false;
+                end = System.currentTimeMillis();
+                System.out.println("all ping = " + (N + M));
+                System.out.println("all time = " + (end - run) + " msec");
+                System.out.println("ping with response = " + N);
+                System.out.println("ping with not response = " + M);
+                System.out.println("Average Packets Per Second = " + 1000 * (M + N) / (end - run) + " pps");
+            }
+        }
+    }
+
+}
